879b26642d7cc486b2090b4d33e85e5e5cf7f18e
yuri0171p-bit <yuri0171p@gmail.com>
2026-05-12 21:14:47 -0300

    Update facebook.js

--- api/facebook.js
+++ api/facebook.js
@@ -1,1 +1,22 @@
-
+// api/facebook.js
+module.exports = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') { res.writeHead(200); return res.end(); }
+    const data = {
+        android_dialog_configs: {},
+        android_sdk_error_categories: [],
+        gdpv4_nux_content: {},
+        gdpv4_nux_enabled: false,
+        id: ":443",
+        supports_implicit_sdk_logging: true
+    };
+    const body = JSON.stringify(data);
+    res.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Content-Length': Buffer.byteLength(body),
+        'Server': 'Garena-Lobby-Server',
+        'Connection': 'close',
+        'Access-Control-Allow-Origin': '*'
+    });
+    res.end(body);
+};
