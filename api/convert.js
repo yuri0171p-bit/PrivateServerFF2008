7d16b74714c60d54d8c5e0f311ce37611954cfd4
yuri0171p-bit <yuri0171p@gmail.com>
2026-05-12 21:16:47 -0300

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,1 +1,7 @@
-
+// api/convert.js
+module.exports = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') { res.writeHead(200); return res.end(); }
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ status: "ok" }));
+};
